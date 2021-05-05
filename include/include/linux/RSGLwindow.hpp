ef2a8e7df9632f15c7ea0141d41ac06514d6911d
Comrade Riley <50218871+ColleagueRiley@users.noreply.github.com>
2021-05-05 18:08:22 -0400

    Add files via upload

--- include/include/linux/RSGLwindow.hpp
+++ include/include/linux/RSGLwindow.hpp
@@ -1,27 +1,25 @@
 #pragma once
-#include <X11/Xlib.h>
-//#include <iostream>
+#include <iostream>
 #include "rsgl.hpp"
 
 struct RSGLWindow{
     Window window;
-    const char* name;
+    std::string name;
     
     int x;
     int y;
     int w;
     int h;
-
+    RSGL::color color;
     int setColor(RSGL::color c){
+        color = c;
         XSetWindowBackground(RSGL::display,window,RSGLRGBTOHEX(c.r,c.g,c.b));
         return 1;
     }
 };
 
-
-RSGLWindow createWindow(const char* name,int x1, int y1,int w1, int h1, RSGL::color c){
-    //Opens new connection to X server
-
+RSGLWindow createWindow(std::string name,int x1, int y1,int w1, int h1, RSGL::color c){
+    //extern XVisualInfo* vi = glXChooseVisual(RSGL::display, 0, 0);
     RSGL::window = XCreateSimpleWindow(RSGL::display, RootWindow(RSGL::display, RSGL::screenNumber), x1, y1, w1, h1, 1,
                                  BlackPixel(RSGL::display, RSGL::screenNumber),  RSGLRGBTOHEX(c.r,c.g,c.b));
     long event_mask = ExposureMask
@@ -32,18 +30,19 @@ RSGLWindow createWindow(const char* name,int x1, int y1,int w1, int h1, RSGL::co
                     | FocusChangeMask
                     | PointerMotionMask
                     | SubstructureNotifyMask
-                    | StructureNotifyMask;
+                    | StructureNotifyMask
+                    | ExposureMask;
     XSelectInput(RSGL::display, RSGL::window, event_mask);
     XMapWindow(RSGL::display, RSGL::window);
     if (!RSGL::window){
         return {};
     }
-    XStoreName(RSGL::display, RSGL::window, name);
+    XStoreName(RSGL::display, RSGL::window, name.data());
     XNextEvent(RSGL::display, &RSGL::event);
-    Atom wm_delete = XInternAtom(RSGL::display, "WM_DELETE_WINDOW", True);
+    Atom wm_delete = XInternAtom(RSGL::display, "WM_DELETE_WINDOW", true);
     XSetWMProtocols(RSGL::display, RSGL::window, &wm_delete, 1);
     XGCValues values;
     RSGL::gc = DefaultGC(RSGL::display, 0);
     
-    return {RSGL::window,name,x1,x1,w1,w1};
+    return {RSGL::window,name,x1,x1,w1,w1,.color=c};
 }
