0e9d18cb745f50c674075e4260d2c5bc4488f1ce
Colleague Riley <50218871+ColleagueRiley@users.noreply.github.com>
2021-05-22 00:36:33 -0400

    Add files via upload

---
+++ include/include/windows/rsgl.hpp
@@ -0,0 +1,3 @@
+#include <iostream>
+
+std::string wintest(){return "windows gay";}
--- include/linux.hpp
+++ include/linux.hpp
@@ -1,5 +1,7 @@
 #include <X11/Xlib.h>
 
-#include "include/linux/RSGLwindow.hpp"
 #include "include/linux/rsgl.hpp"
+#include "include/linux/RSGLwindow.hpp"
 #include "include/linux/keys.hpp"
+#include "include/linux/opengl.hpp"
+#include "include/linux/collision.hpp"
